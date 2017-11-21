61ecf946df987dc1418a6f46eefbfb51092fe4a9
Ashley Johnson <ashvalejohn@gmail.com>
2017-11-21 15:00:38 -0800

    Add static pages controller

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,8 @@
+class Api::SessionsController < ApplicationController
+  def create
+  end
+
+  def destroy
+    
+  end
+end
---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,15 @@
+class Api::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+    else
+      render json: @user.errors.full_messages
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
