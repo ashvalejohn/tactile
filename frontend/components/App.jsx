c0c7e932695b07e6f7a19333b3b29b9971171de5
Ashley Johnson <ashvalejohn@gmail.com>
2017-11-21 17:32:01 -0800

    Render App ✨

---
+++ frontend/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>If you’re seeing this, the App component has rendered.</h1>
+    <img alt="success-gif" src="https://media.giphy.com/media/fBZXu9v0qjjTq/giphy.gif" />
+  </div>
+);
+
+
+export default App;
