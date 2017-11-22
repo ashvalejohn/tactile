fa2d50583252c8ac6446e090b8b614b36ad3f7e2
Ashley Johnson <ashvalejohn@gmail.com>
2017-11-22 13:47:12 -0800

    Style login page

--- frontend/components/session/login.jsx
+++ frontend/components/session/login.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-// import Modal from 'react-modal';
+import Modal from 'react-modal';
 
 class Login extends Component {
   constructor(props) {
@@ -26,27 +26,31 @@ class Login extends Component {
 
   render() {
     return (
-      <div>
-        <h2>Log In</h2>
-        <form action="">
-          <label htmlFor="login-email">Email
-            <input
-              id="login-email"
-              type="text"
-              onChange={this.handleChange('email')}
-            />
-          </label>
-          <label htmlFor="login-password">Password
-            <input
-              id="login-password"
-              type="text"
-              onChange={this.handleChange('password')}
-            />
-          </label>
-          <h3>Don’t have an account? <Link to='/signup'>Sign up</Link>.</h3>
-          <button onClick={this.handleSubmit}>Log In</button>
+      <Modal isOpen={true} className="modal" overlayClassName="overlay">
+        <form className="auth-form" action="">
+          <h2>Log In</h2>
+          <fieldset className="auth-form__fieldset">
+            <label className="auth-form__label" htmlFor="login-email">Email
+              <input
+                id="login-email"
+                type="text"
+                onChange={this.handleChange('email')}
+              />
+            </label>
+            <label className="auth-form__label" htmlFor="login-password">Password
+              <input
+                id="login-password"
+                type="text"
+                onChange={this.handleChange('password')}
+              />
+            </label>
+          </fieldset>
+          <div className='modal-button'>
+            <h3>Don’t have an account? <Link to='/signup'>Sign up</Link>.</h3>
+            <button onClick={this.handleSubmit}>Log In</button>
+          </div>
         </form>
-      </div>
+      </Modal>
     );
   }
 }
