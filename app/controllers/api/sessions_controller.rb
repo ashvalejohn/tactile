c6fb552ffde5c751a5a942252ebcf7813a959c76
Ashley Johnson <ashvalejohn@gmail.com>
2017-11-21 16:16:10 -0800

    Setup backend auth

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,8 +1,26 @@
 class Api::SessionsController < ApplicationController
   def create
+    @user = User.find_by_credentials(user_params[:email], user_params[:password])
+    if @user
+      login(@user)
+      render "api/users/show"
+    else
+      render json: ["Invalid email or password"], status: 422
+    end
   end
 
   def destroy
-    
+    @user = current_user
+    if @user
+      logout
+    else
+      render json: ["No one has signed in yet"], status: 404
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,28 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates :email, :password_digest, :session_token, presence: true
   validates :password, length: { minimum: 6 }, allow_nil: true
+
+  after_validation :ensure_session_token
+  attr_reader :password
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil unless user
+    BCrypt::Password.new(user.password_digest).is_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
 end
