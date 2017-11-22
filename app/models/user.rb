a2afdbde6852ba60b707183916bdba52a7b9494d
Ashley Johnson <ashvalejohn@gmail.com>
2017-11-22 10:23:30 -0800

    Change after_validation to before_validation in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   validates :email, :password_digest, :session_token, presence: true
   validates :password, length: { minimum: 6 }, allow_nil: true
 
-  after_validation :ensure_session_token
+  before_validation :ensure_session_token
   attr_reader :password
 
   def self.find_by_credentials(email, password)
