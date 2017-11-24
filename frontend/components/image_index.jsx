2cbdbebecd5c923da7e9b7d3508a096bb100f407
Ashley Johnson <ashvalejohn@gmail.com>
2017-11-24 11:38:27 -0800

    Render image index 👏🏼

--- frontend/components/image_index.jsx
+++ frontend/components/image_index.jsx
@@ -1,21 +1,15 @@
 import React, { Component } from 'react';
 
 class ImageIndex extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
   componentDidMount() {
-    this.setState({ images: this.props.photoRoll() });
-    console.log("******");
-    console.log(this.state.images);
+    this.props.fetchImages();
   }
 
   render() {
-    console.log(this.state.images);
     return (
-      <img alt="Test outfit" src="http://res.cloudinary.com/ashvalejohn/image/upload/v1511394433/2017-dec1-lwl-img5_uca9mz.jpg" />
+      <div>
+        <img alt="" src={this.props.images.image_url} />
+      </div>
     );
   }
 }
--- frontend/reducers/image_reducer.js
+++ frontend/reducers/image_reducer.js
@@ -2,13 +2,9 @@ import { RECEIVE_IMAGES } from '../actions/image_actions';
 
 const ImageReducer = (state = {}, action) => {
   Object.freeze(state);
-  const images = {};
   switch (action.type) {
     case RECEIVE_IMAGES:
-      action.images.forEach((image) => {
-        images[image.id] = image;
-      });
-      return images;
+      return action.images;
     default:
       return state;
   }
