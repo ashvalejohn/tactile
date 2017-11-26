671afc42efe5eda57ebcf5055ea5bb5449660b04
Ashley Johnson <ashvalejohn@gmail.com>
2017-11-26 13:55:58 -0800

    Add image_maps to images as json from controller

--- app/controllers/api/images_controller.rb
+++ app/controllers/api/images_controller.rb
@@ -1,6 +1,6 @@
 class Api::ImagesController < ApplicationController
   def index
-    @images = Image.all
-    render :index
+    @images = Image.all.to_json({:include => :image_maps})
+    render json: @images 
   end
 end
