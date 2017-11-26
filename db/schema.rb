e521b07687809b1f06a848810e6423098fd4c612
Ashley Johnson <ashvalejohn@gmail.com>
2017-11-26 10:49:09 -0800

    Create items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171126180014) do
+ActiveRecord::Schema.define(version: 20171126184316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20171126180014) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "image_maps", default: [], array: true
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "item_image_url", null: false
+    t.string "description", null: false
+    t.decimal "price", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
