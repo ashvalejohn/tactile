23b99f02960a07ef725234a6b2422b7eb7fe1f5a
Ashley Johnson <ashvalejohn@gmail.com>
2017-11-27 15:03:39 -0800

    Adjust item detail panel styling

--- frontend/components/item/item_detail.jsx
+++ frontend/components/item/item_detail.jsx
@@ -32,26 +32,28 @@ class ItemDetail extends Component {
     const item = this.props.item; 
 
     return (
-      <div className="item-detail__panel">
+      <div className="item-detail-panel">
         <div className="item-detail">
-          <h1 className="item-title">{item.description}</h1>
-          <div className="item-img-container">
-            <img className="item-img" src={item.item_image_url} alt="" />
+          <div className="item-detail-info">
+            <h1 className="item-title">{item.description}</h1>
+            <div className="item-img-container">
+              <img className="item-img" src={item.item_image_url} alt="" />
+            </div>
+            <h2 className="item-price">{item.price}</h2>
+            <form className="item-sizes">
+              {
+                item.sizes.map(size => (
+                  <label key={`${item.id}${size}`}>
+                    <input type="radio" value={size} name="item-size"/>
+                    <span className="size-text">{size}</span>
+                  </label>
+                ))
+              }
+            </form>
+            <Link to="/cart">
+              <button className="add-to-cart">Add to Cart</button>
+            </Link>
           </div>
-          <h2 className="item-price">{item.price}</h2>
-          <form className="item-sizes">
-            {
-              item.sizes.map(size => (
-                <label key={`${item.id}${size}`}>
-                  <input type="radio" value={size} name="item-size"/>
-                  <span className="size-text">{size}</span>
-                </label>
-              ))
-            }
-          </form>
-          <Link to="/cart">
-            <button className="add-to-cart">Add to Cart</button>
-          </Link>
         </div>
         <div className="item-detail__overlay" onClick={this.clickAway}></div>
       </div>
