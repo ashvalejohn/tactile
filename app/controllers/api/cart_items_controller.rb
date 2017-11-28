76c25fb84f0cf10167e13a8a96f808763ac04efe
Ashley Johnson <ashvalejohn@gmail.com>
2017-11-28 15:27:23 -0800

    Get user_id from current_user insead of state.currentUser

--- app/controllers/api/cart_items_controller.rb
+++ app/controllers/api/cart_items_controller.rb
@@ -1,6 +1,7 @@
 class Api::CartItemsController < ApplicationController
   def create
     @cart_item = CartItem.new(cart_item_params)
+    @cart_item.user_id = current_user.id
     if @cart_item.save
       render :index
     else
@@ -29,7 +30,7 @@ class Api::CartItemsController < ApplicationController
 
   private
   def cart_item_params
-    params.require(:cartItem).permit(:item_id, :user_id, :size, :quantity)
+    params.require(:cartItem).permit(:item_id, :size, :quantity)
   end
 
 end
