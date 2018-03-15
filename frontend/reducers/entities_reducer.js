bf979e34ca75b105d4e114d8db575dd76120e204
Ashley Johnson <ashvalejohn@gmail.com>
2018-03-14 18:32:40 -0700

    Create redux cycle for search

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import imageReducer from './image_reducer';
 import itemReducer from './item_reducer';
 import cartItemReducer from './cart_item_reducer';
+import searchReducer from './search_reducer';
 
 const entitiesReducer = combineReducers({
   images: imageReducer,
   item: itemReducer,
   cartItems: cartItemReducer,
+  search: searchReducer,
 });
 
 export default entitiesReducer;
--- frontend/util/search_api_util.js
+++ frontend/util/search_api_util.js
@@ -1,9 +1,9 @@
-const fetchSearchResults = search => (
+export const RECEIVE_SEARCH_TERM = 'RECEIVE_SEARCH_TERM';
+
+export const fetchSearchResults = search => (
   $.ajax({
     method: 'POST',
     url: '/api/items/search',
     data: { search },
   })
 );
-
-export default fetchSearchResults;
